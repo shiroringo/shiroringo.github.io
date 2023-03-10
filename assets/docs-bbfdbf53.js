40825a3fe214684e12a3fe98705743fc8dfc531d
喵 <ringo@miaopuc.com>
2023-03-10 17:56:50 +0800

    feat: bot使用说明文档初版

--- assets/docs-bbfdbf53.js
+++
@@ -1,8 +0,0 @@
-import{e as r,r as o,o as t,i as s,u as c}from"./index-2673a5f2.js";const a=`### 如何使用\r
-\r
-#### 基础功能（不绑ck）\r
-面板功能，戳一戳功能\r
-#### 进阶\r
-私发\r
-#### 其他（圆神无关）\r
-卧槽`,i=r({__name:"docs",setup(_){const n=a;return(p,m)=>{const e=o("v-md-preview");return t(),s(e,{text:c(n)},null,8,["text"])}}});export{i as default};
