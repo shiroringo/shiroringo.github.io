ade54683172fa34a7855bba90d42d20b4278d96d
喵 <ringo@miaopuc.com>
2023-03-10 14:08:03 +0800

    fix: 修复页面空白的问题

--- assets/docs-f2aa7ebc.js
+++
@@ -1,8 +0,0 @@
-import{e as r,i as o,u as t,r as s,o as c}from"./index-2982d858.js";const a=`### 如何使用\r
-\r
-#### 基础功能（不绑ck）\r
-面板功能，戳一戳功能\r
-#### 进阶\r
-私发\r
-#### 其他（圆神无关）\r
-卧槽`,i=r({__name:"docs",setup(_){const n=a;return(p,m)=>{const e=s("v-md-preview");return c(),o(e,{text:t(n)},null,8,["text"])}}});export{i as default};
